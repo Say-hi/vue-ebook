5329b43c9ff400f1370d8c07a89e577547458837
Say-hi <782108446@qq.com>
2019-11-07 11:15:38 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <span class="icon-bookmark"></span>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style scoped>
+.icon-bookmark {
+  font-size: 60px;
+  color: red;
+}
+</style>
